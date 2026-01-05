a3d7442f998736890190560487fd019441c9feb6
Shalinisinha22 <shalinisinha.cspatna@gmail.com>
2026-01-05 18:07:17 +0530

    api_updated

--- src/components/CategoryManagement.jsx
+++ src/components/CategoryManagement.jsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
+
 // Backend API URL
-const API_URL = 'http://localhost:5000/api'
+const API_URL = import.meta.env.VITE_API_URL
 
 // Helper function to get headers with token
 function getHeaders() {
--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 
 // Backend API URL
-const API_URL = "http://localhost:5000/api"
+const API_URL = import.meta.env.VITE_API_URL
 console.log(API_URL)
 
 // Helper function to get headers with token
